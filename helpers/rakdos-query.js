2f8c011bca14e2ced6034c4e373081ee39062b59
Filipe Kiss <hello@filipekiss.com.br>
2018-09-19 08:25:47 -0300

    Add Token Support in RakdosQuery

--- helpers/rakdos-query.js
+++ helpers/rakdos-query.js
@@ -1,13 +1,19 @@
 const sets = require('./sets.json');
 
 class RakdosQuery {
+    static get TOKEN() {
+        return 'token';
+    }
+
     constructor(queryString) {
         this.originalString = queryString;
         this.isPrice = false;
         this.isArt = false;
         this.isLegality = false;
+        this.isToken = false;
         let query = this.parseModifiers(queryString);
         query = this.parseSet(query);
+        query = this.parseToken(query);
         this.text = query;
     }
 
@@ -45,6 +51,29 @@ class RakdosQuery {
                 }
                 return true;
             })
+            .join(' ');
+    }
+
+    parseToken(string) {
+        const debug = (item) => console.log(item) || item;
+        return string
+            .split(' ')
+            .map(debug)
+            .filter((segment) => {
+                const isToken = segment.toLowerCase() === RakdosQuery.TOKEN;
+                console.log(segment.toLowerCase());
+                console.log(RakdosQuery.TOKEN);
+                console.log(isToken);
+                if (isToken) {
+                    this.isToken = isToken;
+                    if (this.set) {
+                        this.set = `t${this.set}`;
+                    }
+                    return false;
+                }
+                return true;
+            })
+            .map(debug)
             .join(' ');
     }
 }
