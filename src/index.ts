97b25017cd4a6dc2132ca4b05ec1c6793802cd35
Filipe Kiss <hello@filipekiss.com.br>
2018-09-24 12:25:43 -0300

    Add TypeScript Support
     - Use Babel with TypeScript
     - Rename all `src/**.js` files to `src/**/*.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 const express = require('express');
-const bot = require('./bot');
+const rakdosBot = require('./bot');
 
 const expressApp = express();
 
-expressApp.use(bot.webhookCallback(`/bot/${process.env.BOT_TOKEN}`));
+expressApp.use(rakdosBot.webhookCallback(`/bot/${process.env.BOT_TOKEN}`));
 expressApp.get('/', (req, res) => {
     res.send(
         '<h1>Rakdos Bot - See <a href="https://github.com/filipekiss/rakdos">the github repo</a> for issues and suggestions</h1>'
@@ -11,7 +11,7 @@ expressApp.get('/', (req, res) => {
 });
 expressApp.get('/set-hook', (req, res) => {
     const fullUrl = `https://${req.get('host')}`;
-    bot.telegram.setWebhook(`${fullUrl}/bot/${process.env.BOT_TOKEN}`);
+    rakdosBot.telegram.setWebhook(`${fullUrl}/bot/${process.env.BOT_TOKEN}`);
     res.send(`WebHook set to ${fullUrl}/bot/:token`);
 });
 expressApp.listen(3000);
---
+++ src/test.ts
@@ -0,0 +1,1 @@
+console.log(`Bot Token: ${process.env.BOT_TOKEN}`);
